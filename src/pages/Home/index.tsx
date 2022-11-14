6714a842910f672f3428a8c1250becbf5e4deeb9
MiguelMorenoBarbosa <miguelmorenobarbosa7@gmail.com>
2022-11-14 16:07:24 -0300

    Tentando

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import principal from "assets/img/principal.png";
 import * as S from "./styles";
 import { CardComponent } from "components";
-import dados from "../../services/dados";
-
+import dados from "services/dados";
+import { IMessageData } from "interfaces/message.interface";
+import { apiMessage } from "services/data";
 
 const Home = () => {
+  const [messages, setMessage] = useState<IMessageData[]>()
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await apiMessage.index()
+      setMessage(response.data)
+    }
+    fetchData()
+  }, [])
   return (
     <S.Home>
       <picture>
